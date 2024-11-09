dbe9e01a7b9da5600fb229871681a7e30c9e0391
AdityaUniyal5678 <adityauniyal5678@gmail.com>
2024-11-09 05:31:52 +0530

    responsive

--- carousel.php
+++ carousel.php
@@ -1,48 +1,52 @@
 <div class="container">
     <div class="row justify-content-center">
- <br>
-</div>
-<div class="row shadow-lg p-3 mb-5 bg-dark rounded">
-
-  <img class="col-3" src="images/index/logo.png"  alt="">
-
-<div id="carouselExampleIndicators" class="carousel slide col-6" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="images/index/2.png" alt="First slide">
-      <div class="carousel-caption d-none d-md-block">
-    <h3 class=".text-dark">DONATE</h3>
-   
-  </div>
-
+ 
     </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="images\index\3.jpg" alt="Second slide">
-      <div class="carousel-caption d-none d-md-block">
-    <h3 class=".text-dark">BRING SMILES</h3>
-   
-  </div>
+    <div class="row shadow-lg p-3 mb-5 bg-dark rounded">
+        <div class="col-md-3 d-none d-md-block">   <!-- Hide on smaller screens -->
+            <img src="images/index/logo.png" alt="" class="img-fluid">
+        </div>
+        <div class="col-12 col-md-6">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src="images/index/2.png" alt="First slide">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h3 class="text-dark">DONATE</h3>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="images/index/3.jpg" alt="Second slide">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h3 class="text-dark">BRING SMILES</h3>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="images/index/4.jpg" alt="Third slide">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h3 class="text-dark">SPREAD LOVE</h3>
+                        </div>
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+        <div class="col-md-3 d-none d-md-block">   <!-- Hide on smaller screens -->
+            <img src="images/index/logo.png" alt="" class="img-fluid">
+        </div>
     </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="images\index\4.jpg" alt="Third slide">
-      <div class="carousel-caption d-none d-md-block">
-    <h3 class=".text-dark">SPREAD LOVE</h3>
-  </div>
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
 </div>
-<img class="col-3" src="images/index/logo.png"  alt="">
-</div>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
